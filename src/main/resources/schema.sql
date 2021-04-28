4935d5d35877d76af143c43953be7eb98d6f7ddc
antongrutsin <Risenka13524>
2021-04-28 22:27:20 +0300

    Init

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,26 @@
+CREATE TABLE `spring_data_db`.`product` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `cost` INT NULL,
+  PRIMARY KEY (`id`));
+
+INSERT INTO `spring_data_db`.`product` (name, cost) VALUES ('Milk', 78);
+INSERT INTO `spring_data_db`.`product` (name, cost) VALUES ('Eggs', 125);
+INSERT INTO `spring_data_db`.`product` (name, cost) VALUES ('Potatos', 34);
+INSERT INTO `spring_data_db`.`product` (name, cost) VALUES ('Blades', 178);
+INSERT INTO `spring_data_db`.`product` (name, cost) VALUES ('Beer', 90);
+INSERT INTO `spring_data_db`.`product` (name, cost) VALUES ('Bread', 28);
+INSERT INTO `spring_data_db`.`product` (name, cost) VALUES ('Tea', 200);
+INSERT INTO `spring_data_db`.`product` (name, cost) VALUES ('Coffee', 78);
+INSERT INTO `spring_data_db`.`product` (name, cost) VALUES ('Paper', 378);
+INSERT INTO `spring_data_db`.`product` (name, cost) VALUES ('Tobacco', 219);
+INSERT INTO `spring_data_db`.`product` (name, cost) VALUES ('Ketchup', 88);
+INSERT INTO `spring_data_db`.`product` (name, cost) VALUES ('Water', 39);
+INSERT INTO `spring_data_db`.`product` (name, cost) VALUES ('Coffee Machine', 2478);
+INSERT INTO `spring_data_db`.`product` (name, cost) VALUES ('Whiskas', 19);
+INSERT INTO `spring_data_db`.`product` (name, cost) VALUES ('Book', 1178);
+INSERT INTO `spring_data_db`.`product` (name, cost) VALUES ('Cleaner', 457);
+INSERT INTO `spring_data_db`.`product` (name, cost) VALUES ('Shoes', 3478);
+INSERT INTO `spring_data_db`.`product` (name, cost) VALUES ('Light Bulb', 78);
+INSERT INTO `spring_data_db`.`product` (name, cost) VALUES ('Carrot', 48);
+INSERT INTO `spring_data_db`.`product` (name, cost) VALUES ('Onion', 38);
